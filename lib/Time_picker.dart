372c3ed9823343ce9b63a01138b046eb8f4aa51f
kimpyeongan <80189410+kimpyeongan@users.noreply.github.com>
2021-11-11 00:54:17 +0900

    Create Time_picker.dart

---
+++ lib/Time_picker.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _selectedTime;
+
+  // We don't need to pass a context to the _show() function
+  // You can safety use context as below
+  Future<void> _show() async {
+    final TimeOfDay? result =
+    await showTimePicker(context: context, initialTime: TimeOfDay.now());
+    if (result != null) {
+      setState(() {
+        _selectedTime = result.format(context);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          _selectedTime != null ? _selectedTime! : 'No time selected!',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton:
+      ElevatedButton(onPressed: _show, child: Text('Show Time Picker')),
+    );
+  }
+}
